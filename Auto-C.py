d716fcb4d091304ad681be7435340d9d2001ff00
B-Panther <150675493+B-Panther@users.noreply.github.com>
2025-03-09 14:21:57 +0500

    Update Auto-C.py

--- Auto-C.py
+++ Auto-C.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-import AUT_CRT2
+import AUTYT2
 import time
 import sys
 from itertools import cycle
@@ -41,10 +41,10 @@ try:
         subprocess.run(["xdg-open", link])  # For Linux or macOS, this will open the URL in the default browser
 
     print("▶️ Running main function...")
-    AUT_CRT2.main()  # Assumes 'main' exists; will raise an error if it doesn't.
+    AUTYT2.main()  # Assumes 'main' exists; will raise an error if it doesn't.
 
 except ImportError as e:
     print("❌ Import error:", e)
 
 except AttributeError:
-    print("⚠️ 'main' function not found. Check available attributes:", dir(AUT_CRT2))
+    print("⚠️ 'main' function not found. Check available attributes:", dir(AUTYT2))
